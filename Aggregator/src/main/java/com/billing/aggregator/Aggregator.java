08f83ce4017dca65721d69055d4b1e93c14197d1
Medhat31 <medhatkhalil340@gmail.com>
2026-06-27 23:11:44 +0300

    feat: Started aggregator implementation and added DatabaseAdaptor implementation

---
+++ Aggregator/src/main/java/com/billing/aggregator/Aggregator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.billing.aggregator;
+
+/**
+ *
+ * @author mfathy
+ */
+public class Aggregator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
