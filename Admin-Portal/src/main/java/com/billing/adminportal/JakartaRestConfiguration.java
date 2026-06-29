942766bfd1d6d2cea1a3e282b4ec7a3a2b0a1237
seifabsalam <seif.absalam@gmail.com>
2026-06-29 09:51:42 +0300

    Website Initial Commit

---
+++ Admin-Portal/src/main/java/com/billing/adminportal/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.billing.adminportal;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("billing")
+public class JakartaRestConfiguration extends Application {
+    
+}
